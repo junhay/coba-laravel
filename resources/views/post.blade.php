0579207df5d0d29d6306781214892a39249fb7e3
Jun Hay Laptop <tanjunhay@gmail.com>
2022-06-26 06:18:20 +0700

    Push Perdana

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+    <article>
+        <h2>Judul</h2>
+        <h5>Author</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum asperiores, voluptatem vero veniam cupiditate vitae maxime explicabo distinctio maiores molestiae suscipit sit dolor velit obcaecati eveniet, voluptatum numquam ipsam ut cum fuga voluptas quod provident sint? Totam natus pariatur temporibus sapiente culpa neque aliquid nesciunt esse, nam repellat necessitatibus labore atque deserunt, qui voluptate delectus suscipit mollitia quas. Voluptatum deserunt debitis dolore atque reiciendis corrupti aliquid optio ea iure, ab commodi! Eos possimus aperiam quas ipsum laborum harum suscipit.</p>
+    </article>
+    <a href="/blog">Back to Posts</a>
+@endsection
