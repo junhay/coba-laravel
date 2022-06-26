1fb83547034daa2815004532ac1af6fa6debc2e7
Jun Hay <tanjunhay@gmail.com>
2022-06-26 20:47:16 +0700

    coba-laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,9 @@
 
 @section('container')
     <article>
-        <h2>Judul</h2>
-        <h5>Author</h5>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum asperiores, voluptatem vero veniam cupiditate vitae maxime explicabo distinctio maiores molestiae suscipit sit dolor velit obcaecati eveniet, voluptatum numquam ipsam ut cum fuga voluptas quod provident sint? Totam natus pariatur temporibus sapiente culpa neque aliquid nesciunt esse, nam repellat necessitatibus labore atque deserunt, qui voluptate delectus suscipit mollitia quas. Voluptatum deserunt debitis dolore atque reiciendis corrupti aliquid optio ea iure, ab commodi! Eos possimus aperiam quas ipsum laborum harum suscipit.</p>
+        <h2>{{ $post["title"] }}</h2>
+        <h5>{{ $post["author"] }}</h5>
+        <p>{{ $post["body"] }}</p>
     </article>
     <a href="/blog">Back to Posts</a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,29 +30,23 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Jun Hay",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus itaque saepe natus quia eum dolorem cupiditate numquam, incidunt sint voluptatem voluptatum obcaecati veniam ullam fuga, praesentium reiciendis! Hic eius earum, similique magnam amet id quis beatae laborum maiores eveniet nesciunt soluta minus dicta, facere alias incidunt saepe commodi fugiat. Expedita reprehenderit quis cumque dolores suscipit tempora fugiat ratione non facere quod laudantium cupiditate, earum assumenda repudiandae natus voluptatem sit accusamus, rem, rerum molestiae dolor! Cumque aliquam obcaecati hic quidem praesentium?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Martina",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit unde soluta tempore expedita voluptatibus quae, necessitatibus aliquam placeat dicta a animi amet quibusdam impedit illo consequatur, veritatis veniam ad odio aut. Nemo, veniam ut. Delectus quo sequi deserunt accusantium ullam, odio, adipisci, molestiae aliquam earum illum similique assumenda inventore consequatur! Ut, perferendis sint. At possimus est, maxime quo ratione aut in harum qui magni fugit provident voluptatum officia dolor tenetur quis, rem recusandae veniam ad? A accusamus aliquid soluta rerum, nisi at saepe omnis fugit recusandae natus, modi vel accusantium ex! Quos eligendi nulla reprehenderit voluptatum, laboriosam ipsam ipsa amet?"
-        ]
-    ];
     return view('posts',[
         "title" => "Posts",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 //halaman single posts
 Route::get('posts/{slug}', function ($slug) {
+
+    // $new_post = [];
+    // foreach ($blog_posts as $post) {
+    //     if($post["slug"] === $slug){
+    //         $new_post = $post;
+    //     }
+    // }
     return view('post',[
-        "title" => "Single Post"
+        "title" => "Single Post",
+        "post" => Post::find($slug)
     ]);
 });
