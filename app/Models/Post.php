4ef8944787c3bdd92059255459beaccc81dc3c37
Jun Hay <tanjunhay@gmail.com>
2022-07-28 06:41:18 +0700

    tutor 7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Jun Hay",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus itaque saepe natus quia eum dolorem cupiditate numquam, incidunt sint voluptatem voluptatum obcaecati veniam ullam fuga, praesentium reiciendis! Hic eius earum, similique magnam amet id quis beatae laborum maiores eveniet nesciunt soluta minus dicta, facere alias incidunt saepe commodi fugiat. Expedita reprehenderit quis cumque dolores suscipit tempora fugiat ratione non facere quod laudantium cupiditate, earum assumenda repudiandae natus voluptatem sit accusamus, rem, rerum molestiae dolor! Cumque aliquam obcaecati hic quidem praesentium?",
-        ],
-        [
-            "title" => "Judul Post Test",
-            "slug" => "judul-post-kedua",
-            "author" => "Martina",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit unde soluta tempore expedita voluptatibus quae, necessitatibus aliquam placeat dicta a animi amet quibusdam impedit illo consequatur, veritatis veniam ad odio aut. Nemo, veniam ut. Delectus quo sequi deserunt accusantium ullam, odio, adipisci, molestiae aliquam earum illum similique assumenda inventore consequatur! Ut, perferendis sint. At possimus est, maxime quo ratione aut in harum qui magni fugit provident voluptatum officia dolor tenetur quis, rem recusandae veniam ad? A accusamus aliquid soluta rerum, nisi at saepe omnis fugit recusandae natus, modi vel accusantium ex! Quos eligendi nulla reprehenderit voluptatum, laboriosam ipsam ipsa amet?",
-        ],
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -178,8 +178,8 @@ class ComposerStaticInit0a7f4e016ba9537dcbab1005ca4a4ce7
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Whoops\\' => 
         array (
@@ -335,8 +335,8 @@ class ComposerStaticInit0a7f4e016ba9537dcbab1005ca4a4ce7
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -508,6 +508,7 @@ class ComposerStaticInit0a7f4e016ba9537dcbab1005ca4a4ce7
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\PostController' => __DIR__ . '/../..' . '/app/Http/Controllers/PostController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
@@ -517,6 +518,7 @@ class ComposerStaticInit0a7f4e016ba9537dcbab1005ca4a4ce7
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Post' => __DIR__ . '/../..' . '/app/Models/Post.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
